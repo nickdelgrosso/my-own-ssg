fbb4407b1b028e8c88cf1b31bfafeeadcff0d50d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-10-13 11:28:57 +0200

    aa

--- gen.py
+++ gen.py
@@ -5,9 +5,8 @@ from utils import load_config, render_theme
 
 config = load_config('config.yml')
 
-rendered = render_theme(theme_path=config['theme'], content_path='content')
 makedirs('rendered', exist_ok=True)
-for path, html in rendered:
+for path, html in render_theme(theme_path=config['theme'], content_path='content'):
     (Path('rendered') / path).write_text(html)
 
 
